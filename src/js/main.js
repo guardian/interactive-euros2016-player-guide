045cd8236882c5d3534db0e1fa989e3585b8a1db
daanlouter <daan.louter@gmail.com>
2016-05-17 15:01:12 +0100

    lazyloading json

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ import Handlebars from 'handlebars'
 
 import mainHTML from './text/main.html!text'
 import teamHTML from './text/teamPage.html!text'
+import playerCircleHTML from './text/playerCircle.html!text'
 import playerDetailHTML from './text/playerDetailPage.html!text'
 
 var data = {};
@@ -13,14 +14,55 @@ var currentTeam = "England";
 var windowWidth = window.innerWidth;
 var isMobile = windowWidth < 980 ? true : false;
 var onStart = true;
+var projectAssetpath;
+var keyString = {};
+var dataSources = {
+    "England": "1Zsw-NAT-8xtXSQ8t-X3eyisqJPv4G-qFFHvmZ1ej0fw",
+    "Germany": "1i4gM_GeUfoSvBXRHEpFAhgz3UHJukeLqxBiGwZtMKfc",
+    "France":"1KAJtvbQhvsvZ2ssIY2_7qiLuOYSmFCJ1E_1eQ0ETLas",
+    "Sweden":"1Fl2BMqcD70ArTn9BCA0i-DTs4kGePKYY9SXcDhcFdcI",
+    "Italy":"1UkRPDfrRNOkyIazXFWERCchz9pU1hu7g7wmTRjywBMM",
+    "Belgium":"1Yh-6uphNjJSbjbXhlzfl8SiYl-w6cTvk51_A_vr9sjE",
+    "Wales" : "1o8MdeEpwI1NQsk7rgVx6qQDhjhjt4foxK1c--tB3DoU",
+    "Rep of Ireland":"12dGYUtIkVrYRw-e0Ht_sjCXxC3qFcoe4ge7iXtKHE6E",
+    "N Ireland":"1bD63bY4jeYtUVg9KFlKRa8C87hHQYKvlYXAxyc-5IMY",
+    "Spain":"14dG-Or6_BhOJQBPcgzQktd6T0w1m93VPYmCEjr-FqMU",
+    "Portugal":"1Tr7SbOKabNyj7-NPsqV-1PnrNlPH46KrZSMev76Mw3s",
+    "Russia":"1dX0AiX3fwQbKrq4KNGCK44ek_g-cUgPao1O_UQJ6e5Y",
+    "Ukraine":"1ddJ0CvPm23g1AB3iK7jRmzqh5f5xGTiunhEmvg7t-aY",
+    "Romania" :"18oqBD3hE61x-7T_UkNMytIK12vDPSeeVYjm5e7afgqM",
+    "Croatia":"1hyDiy7WOTsZar11Z7JQW9F249rKfwjY7pc7uwFdzs7g",
+    "Turkey":"1T976zQpp_kp4wCtZOCw8vDe8l5GQBJDRARaWcFGmFb4",
+    "Albania":"1UPpqsr7WEwnL7sKX3sT2CoJqVAj_JN-M0UZMJpp85-g",
+    "Poland":"13BT44qfZatoB-QqqKOB3jgea8cUQiMb3DpIDIT4kvRw",
+    "Austria":"1I-Z57rFkvo3fIuh5W3ASAq2d3OCujL_PqPEWrZVA-0Q",
+    "Switzerland" : "1ff7gCcNTzEdAbFrFsCIBECneskzX-xjjU8ZpsEhJsj8", 
+    "Czech Republic":"1lMTejNFx6icnonGIi8kQ3W0NAIEW2mu9DUaGTHjZJWA",
+    "Slovakia":"1mV-s921mm6J4ZYRWRLccuKbZTXIUGePE6T--eY_kAQA",
+    "Hungary":"13PX3dUtmYOkjruakrseUoZoUfKcXIDPi9TsMT8WlX6M",
+    "Iceland": "1jie0qY09f8AqezNJqnVXzRpQOd7EcA9xPsyjkRFzHto"
+}
+var masterTeamData = "1We0iF5KgHN0LlQjFn9C9onBrX1ovVJnsbTv9qTKBQn0";
 
-function $(selector){
-    return bonzo(qwery(selector));
+data.teamNames = ["England","Germany","France","Sweden","Italy","Belgium","Wales Rep of Ireland","N Ireland","Spain","Portugal","Russia","Ukraine","Romania Croatia","Turkey","Albania","Poland","Austria","Switzerland", "Czech Republic","Slovakia","Hungary","Iceland"];
+
+function $(selector,context){
+    return bonzo(qwery(selector,context));
 }
 
 export function init(el, context, config, mediator) {
+    projectAssetpath = config.assetPath;
+    if(window.location.search){
+        var string = window.location.search.replace('?','').split('&');
+        string.forEach(function(a){
+            keyString[a.split('=')[0]] = a.split('=')[1];
+        })
+        if(keyString.team){
+            currentTeam = keyString.team;
+        }
+    }
     reqwest({
-        url: 'https://interactive.guim.co.uk/docsdata-test/10WUlJVnZ23A1JmMESn7sOGm0s3WwKHynz-ptV_8b8uQ.json',
+        url: 'https://interactive.guim.co.uk/docsdata-test/' + masterTeamData + '.json',
         type: 'json',
         crossOrigin: true,
         success: (resp) => {
@@ -28,26 +70,12 @@ export function init(el, context, config, mediator) {
             data.isMobile = isMobile;
 
             resp.sheets.Teams.forEach(function(team){
-                var players = resp.sheets[team.Team];
-
-                if(players){
-                    players = players.map(function(player,index){
-                        player.x = Math.round(Math.random()*20);
-                        player.y = Math.round(Math.random()*20);
-                        player.image = player.name.replace(/\s/g,'_').replace(/\'/g,'') + ".jpg";
-                        player.team = team.Team;
-                        player.specialty = player["special player? (eg. key player, promising talent, etc)"];
-                        player.isSpecial = player.specialty ? true : false;
-                        
-                        if(team.Team === currentTeam && index === 0){
-                            player.isActive = true;
-                        }
-
-                        return player
-                    })
-                }else{
-                    players = [];
-                }
+                var players = [
+                    {position:"Forward"},{position:"Forward"},{position:"Forward"},{position:"Forward"},{position:"Forward"},{position:"Forward"},
+                    {position:"Midfielder"},{position:"Midfielder"},{position:"Midfielder"},{position:"Midfielder"},{position:"Midfielder"},{position:"Midfielder"},
+                    {position:"Defender"},{position:"Defender"},{position:"Defender"},{position:"Defender"},{position:"Defender"},{position:"Defender"},{position:"Defender"},{position:"Defender"},
+                    {position:"Goalkeeper"},{position:"Goalkeeper"},{position:"Goalkeeper"}
+                ];
 
                 data.teams.push({
                     "teamName": team.Team,
@@ -69,6 +97,7 @@ export function init(el, context, config, mediator) {
 
 function createPage(el,config){
     Handlebars.registerPartial('teamPage',teamHTML);
+    Handlebars.registerPartial('playerCircle',playerCircleHTML);
     Handlebars.registerPartial('playerDetailPage',playerDetailHTML);
 
     var mainTemplate = Handlebars.compile(mainHTML);
@@ -77,8 +106,6 @@ function createPage(el,config){
     var playerDetailTemplate = Handlebars.compile(playerDetailHTML);
 
     el.innerHTML = mainTemplateParsed;
-
-    console.log(data);
 
     $('.menu-button').each(function(menuButton){
         menuButton.addEventListener('click',function(e){
@@ -90,71 +117,129 @@ function createPage(el,config){
                 $('#detail-overlay-container').removeClass('opened');
                 $('#detail-overlay-container').attr('data-teamname', currentTeam);
                 
-                var teamData = data.teams.filter((team) => team.teamInfo.Team === currentTeam)[0];
-                var teamTemplateRendered = teamTemplate(teamData).replace(/%assetPath%/g,config.assetPath);
-                var detailContainerHTML = '';
-
-                $('#teams-container')[0].innerHTML = teamTemplateRendered;
-                
-                for(var position in teamData.players){
-                    teamData.players[position].forEach(function(player){
-                        var playerDetailTemplateRendered = playerDetailTemplate(player).replace(/%assetPath%/g,config.assetPath);
-                        detailContainerHTML += playerDetailTemplateRendered; 
-                    })
-                }
-
-                $('#detail-overlay-container .detail-container')[0].innerHTML = detailContainerHTML;
-                
-                $('.player-container').each(function(playerEl){
-                    var playerName = playerEl.querySelector('.player-name').innerHTML;
-                    var teamName = playerEl.getAttribute('data-team');
-                    playerEl.addEventListener('click',function(e){
-                       openDetailContainer(playerName,teamName) 
-                    })
-                });
+                loadPlayers(currentTeam)
             }else{
                 var teamEl = $('.team-container[data-teamname="'+ currentTeam +'"]');
                 var teamOffset = teamEl.offset().top - 36;
                 $('body').scrollTop(teamOffset)
-            }
-            
-            
+            }  
         })
     });
 
-    $('.player-container').each(function(playerEl){
-        var playerName = playerEl.querySelector('.player-name').innerHTML;
-        var teamName = playerEl.getAttribute('data-team');
+    if(keyString.team){
+        loadPlayers(keyString.team);
+    }else{
+        loadPlayers("England");
+    }
 
-        if(isMobile){
-            playerEl.addEventListener('click',function(e){
-               openDetailContainer(playerName,teamName) 
-            })
-        }else{
-            playerEl.addEventListener('mouseenter',function(e){
-                var teamData = data.teams.filter((team) => team.teamInfo.Team === teamName)[0];
-                var playerData;
-                for(var key in teamData.players){
-                    if(!playerData){
-                        teamData.players[key].forEach(function(player){
-                            if(player.name === playerName){
-                                playerData = player;
-                            } 
-                        });
-                    }
-                };
-                var playerDetailTemplateRendered = playerDetailTemplate(playerData).replace(/%assetPath%/g,config.assetPath);
-                var playerOffset = $(playerEl).offset().top;
-
-                $('#detail-box-container')[0].innerHTML = playerDetailTemplateRendered;
-                $('#detail-box-container').css('top',playerOffset + 'px');
-                $('.player-container').removeClass('activePlayer')
-                $(playerEl).addClass('activePlayer')
-                createLine(playerOffset,playerEl,playerName)
-            })  
-
+    function scrollInit(){
+        if(!isMobile){
+          var els = document.querySelectorAll('.team-container[data-loaded="false"]');
+          var windowHeight = window.innerHeight;
+          window.addEventListener('scroll', function(e){
+              for(var i=0;i<els.length;i++){
+                  if(els[i].getBoundingClientRect().top < windowHeight * 3){
+                      var teamName = els[i].getAttribute('data-teamname');
+                      loadPlayers(teamName);
+                      els = document.querySelectorAll('.team-container[data-loaded="false"]');
+                  }
+              }
+          });   
         }
-    });
+        
+    }
+
+    function loadPlayers(teamName){
+        var teamData = data.teams.filter((team) => team.teamName === teamName)[0];
+        var teamEl = document.querySelector('.team-container[data-teamname="' + teamName + '"]');
+        if(!isMobile){
+            teamEl.setAttribute('data-loaded','true');
+        }else{
+            teamEl = document.querySelector('#teams-container')
+        }
+        
+        reqwest({
+            url: 'https://interactive.guim.co.uk/docsdata-test/' + dataSources[teamName] + '.json',
+            type: 'json',
+            crossOrigin: true,
+            success: (resp) => {
+                var playerData = resp.sheets.Players;
+                var players = playerData.map(function(player,index){
+                    player.image = player.name.replace(/\s/g,'_').replace(/\'/g,'') + ".jpg";
+                    player.team = teamName;
+                    player.specialty = player["special player? (eg. key player, promising talent, etc)"];
+                    player.isSpecial = player.specialty ? true : false;
+                    
+                    if(teamName === currentTeam && index === 0){
+                        player.isActive = true;
+                    }
+
+                    return player
+                })
+
+                teamData.players = {
+                    "Forwards": players.filter((player)=> player.position === "Forward" || player.position === "Forward (winger)"),
+                    "Midfielders": players.filter((player)=> player.position === "Midfielder"),
+                    "Defenders": players.filter((player)=> player.position === "Defender"),
+                    "Goalkeepers": players.filter((player)=> player.position === "Goalkeeper")
+                }
+                
+                var teamTemplateParsed = teamTemplate(teamData).replace(/%assetPath%/g,projectAssetpath);
+
+                teamEl.innerHTML = teamTemplateParsed;
+                addPlayerEvents(teamEl,teamName,teamData);
+            }
+        })
+    }
+
+    function addPlayerEvents(teamEl,teamName,teamData){
+        $('.player-container',teamEl).each(function(playerEl){
+            var playerName = playerEl.querySelector('.player-name').innerHTML;
+            var teamName = playerEl.getAttribute('data-team');
+
+            if(isMobile){
+                playerEl.addEventListener('click',function(e){
+                   openDetailContainer(playerName,teamName) 
+                })
+            }else{
+                playerEl.addEventListener('mouseenter',function(e){
+                    var playerData;
+                    for(var key in teamData.players){
+                        if(!playerData){
+                            teamData.players[key].forEach(function(player){
+                                if(player.name === playerName){
+                                    playerData = player;
+                                } 
+                            });
+                        }
+                    };
+                    var playerDetailTemplateRendered = playerDetailTemplate(playerData).replace(/%assetPath%/g,config.assetPath);
+                    var playerOffset = $(playerEl).offset().top;
+
+                    $('#detail-box-container')[0].innerHTML = playerDetailTemplateRendered;
+                    $('#detail-box-container').css('top',playerOffset + 'px');
+                    $('.player-container').removeClass('activePlayer')
+                    $(playerEl).addClass('activePlayer')
+                    createLine(playerOffset,playerEl,playerName)
+                })  
+
+            }
+        });
+        
+        if(isMobile){
+           var detailContainerHTML = '';
+           for(var position in teamData.players){
+               teamData.players[position].forEach(function(player){
+                   var playerDetailTemplateRendered = playerDetailTemplate(player).replace(/%assetPath%/g,config.assetPath);
+                   detailContainerHTML += playerDetailTemplateRendered; 
+               })
+           }
+
+           $('#detail-overlay-container .detail-container')[0].innerHTML = detailContainerHTML; 
+        }
+        
+    }
+    
 
     function openDetailContainer(playerName,teamName){
         var playerDetailEl = $('.detail-player-container[data-playername="' + playerName + '"]');
@@ -178,7 +263,6 @@ function createPage(el,config){
         var diff = boxOffset.left - playerOffset.left - playerOffset.width - lineWidth;
         var playerIndex;
 
-
         for(var i=0; i<playerChildEls.length; i++){
             if(playerChildEls[i].getAttribute('data-player') === pName){
                 playerIndex = i + 1;
@@ -201,24 +285,6 @@ function createPage(el,config){
         }
     }
 
-    function createSketches(){
-        var sketchContainer = $('#sketch-container');
-        var projectDimensions = sketchContainer[0].getBoundingClientRect(); 
-        
-
-        for(var i=0;i<200;i++){
-            var block = document.createElement('div');
-            var offset = Math.round(Math.random()*20) * 150;
-            block.className = "sketch-block";   
-
-            block.style.backgroundPosition = "0 -" + offset + "px";
-            block.style.top = Math.random()*projectDimensions.height + "px";
-            block.style.left = Math.random()*(projectDimensions.width - 150) + "px";
-            sketchContainer.append(block)
-
-        }
-    }
-
     if(isMobile){
         $('#detail-overlay-container')[0].addEventListener('click',function(e){
             $(el).removeClass('detail-panel-opened');
@@ -233,4 +299,21 @@ function createPage(el,config){
     }
 
     createSketches();
+    scrollInit();
+}
+
+
+function createSketches(){
+    var sketchContainer = $('#sketch-container');
+    var projectDimensions = sketchContainer[0].getBoundingClientRect(); 
+    
+    for(var i=0;i<180;i++){
+        var block = document.createElement('div');
+        var offset = Math.round(Math.random()*20) * 150;
+        block.className = "sketch-block";   
+        block.style.backgroundPosition = "0 -" + offset + "px";
+        block.style.top = Math.random()*projectDimensions.height + "px";
+        block.style.left = Math.random()*(projectDimensions.width - 150) + "px";
+        sketchContainer.append(block)
+    }
 }
